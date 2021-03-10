ee83a04b177c4adeacf5f61b40b8861637cd12dd
Pau Garcia Martínez <etpau@hotmail.es>
2021-03-10 17:37:04 +0100

    Add fade transition to every page

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/main.css'
   ],
+
+  pageTransition: {
+    name: "fade",
+    mode: "out-in"
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
